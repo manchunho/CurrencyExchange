da1820271903d5b35189ef88af8110d701fb7476
Man Chun Ho <160172305+manchunho@users.noreply.github.com>
2024-02-17 16:20:11 +0000

    Update currencyExchange.py
    - Fix the display of exchange rate
    - Add comment for the code

--- currencyExchange.py
+++ currencyExchange.py
@@ -3,28 +3,32 @@ import tkinter as tk
 import requests
 
 def output():
+    
+    # API request to retrive exchange rate data in json format
     url = 'https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/er-ir/er-eeri-daily?offset=0'
     response = requests.get(url)
-    # retrive response in json format
     data = response.json()
-
-    exchangeRate_In = data['result']['records'][0].get(input_currency.get().lower())
-    exchangeRateNumlb["text"] = exchangeRate_In
     
+    # Input currency
+    exchangeRate_In = data['result']['records'][0].get(input_currency.get().lower())
+    
+    # Output currency
     exchangeRate_Out = data['result']['records'][0].get(output_currency.get().lower())
-    exchangeRateNumlb["text"] = exchangeRate_Out
     
     if (exchangeRate_In is None):
         exchangeRate_In = 1
     if (exchangeRate_Out is None):
         exchangeRate_Out = 1
     
+    # Calculate exchange rate
     result = float(input_cost.get()) * float(exchangeRate_In) / float(exchangeRate_Out)
+    exchangeRateNumlb["text"] = float(exchangeRate_In) / float(exchangeRate_Out)
     output_cost["text"] = result
     
+    # latest date
     latestUpdate = data['result']['records'][0]['end_of_day']
     LatestUpdateValuelb["text"] = latestUpdate
-
+    
 #Tkinter
 win = tk.Tk()
 win.title('Currency Exchange')
@@ -51,7 +55,7 @@ Outputlb.place(x=0 ,y=90)
 output_cost = tk.Label(text="",fg="black",height=1)
 output_cost.place(x=100 ,y=120)
 
-OutputAmountlb = tk.Label(text="Input amount:",fg="black",height=1)
+OutputAmountlb = tk.Label(text="Output amount:",fg="black",height=1)
 OutputAmountlb.place(x=0 ,y=120)
 
 LatestUpdatelb = tk.Label(text="Latest update:",fg="red",height=1)
@@ -60,9 +64,10 @@ LatestUpdatelb.place(x=0 ,y=180)
 LatestUpdateValuelb = tk.Label(text="",fg="red",height=1)
 LatestUpdateValuelb.place(x=90 ,y=180)
 
-#Entry區域
+#Entry
 input_currency = ttk.Combobox(values=["AUD","BEF","CAD","CHF","CNY","DEM","EUR","FRF","GBP","HKD","IDR","INR","ITL",
                                       "JPY","KRW","MYR","NLG","PHP","SGD","THB","TWD","USD","ZAR"])
+input_currency.current(9)
 input_currency.place(x=100, y=0)
 
 input_cost = tk.Entry(width=23)
@@ -71,9 +76,10 @@ input_cost.place(x=100 ,y=30)
 
 output_currency = ttk.Combobox(values=["AUD","BEF","CAD","CHF","CNY","DEM","EUR","FRF","GBP","HKD","IDR","INR","ITL",
                                        "JPY","KRW","MYR","NLG","PHP","SGD","THB","TWD","USD","ZAR"])
+output_currency.current(21)
 output_currency.place(x=100, y=90)
 
-#Button區域
+#Button
 enter_btn = tk.Button(text="Enter",height=1,command=output)
 enter_btn.place(x=150 ,y=150)
 
